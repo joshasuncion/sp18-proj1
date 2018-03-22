c1148965dd6efac176b352bc2b3cce21f7fcd199
Joshua Asuncion <jasuncion@berkeley.edu>
2018-03-22 04:10:53 -0700

    almost done

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,5 +1,23 @@
 class PokemonsController < ApplicationController
 
+  def new
+  end
+
+  def create
+  	pokemon = Pokemon.create(pokemon_params)
+  	trainer = current_trainer
+  	pokemon.trainer = trainer
+  	pokemon.health = 100
+  	pokemon.level = 1
+  	if pokemon.valid? == true
+  	  pokemon.save
+  	  redirect_to trainer_path(trainer)
+  	else
+  	  redirect_to new_path
+  	  flash[:error] = pokemon.errors.full_messages.to_sentence
+  	end
+  end
+
   def capture
     pokemon = Pokemon.find(params[:id])
     trainer = current_trainer
@@ -18,5 +36,11 @@ class PokemonsController < ApplicationController
     end
     redirect_to trainer_path(trainer)
   end
+
+  private
+
+    def pokemon_params
+      params.require(:pokemon).permit(:name, :ndex)
+    end
 
 end
